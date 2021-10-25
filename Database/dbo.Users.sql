6b638a84d400db19230d99b3a752f64369dd1482
Weronika-Buras-WAT <weronika.buras@student.wat.edu.pl>
2021-10-25 23:28:09 +0200

    tables saved

---
+++ Database/dbo.Users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [ID]       INT       NOT NULL IDENTITY,
+    [e-mail]   CHAR (25) NOT NULL,
+    [password] CHAR (25) NOT NULL, 
+    CONSTRAINT [PK_Users] PRIMARY KEY ([ID])
+);
+
