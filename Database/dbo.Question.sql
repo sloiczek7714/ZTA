3b03e7ed3c32d300a7b571b1743efa2845e453db
Weronika-Buras-WAT <65176525+Weronika-Buras-WAT@users.noreply.github.com>
2021-12-16 09:59:09 +0100

    Delete Database directory

--- Database/dbo.Question.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Question] (
-    [ID]            INT       NOT NULL IDENTITY,
-    [Numer pytania] CHAR (10) NOT NULL,
-    [Pytanie]       TEXT      NOT NULL,
-    CONSTRAINT [PK_Question] PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
